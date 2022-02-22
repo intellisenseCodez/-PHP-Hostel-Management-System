ee504ff006474cf21fcd62c67d31d36d03c45c88
Akindutire, Ayomide Samuel <40896417+akindutire@users.noreply.github.com>
2022-02-22 18:10:01 +0100

    changed the connection params

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -3,7 +3,7 @@
 		session_start();
 	}
   
- $conn=mysqli_connect("localhost","root","root","hostel_management_system",3308);
+ $conn=mysqli_connect("localhost","root","","hostel_management_system",3306);
 
   if (!$conn) {
     die("Connection Failed: ".mysqli_connect_error());
